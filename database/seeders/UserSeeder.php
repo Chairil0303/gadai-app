6ddc0cf9be4a933d8ed8c23ac439f7e3f9fa6d2c
chairil0303 <chairilsyahrain24@gmail.com>
2025-03-10 16:55:09 +0700

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'nama' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Pegawai',
+                'email' => 'pegawai@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'pegawai',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
