58cf97f667586d51a10f57904cfac0260747adbe
chairil0303 <chairilsyahrain24@gmail.com>
2025-03-15 11:53:40 +0700

    mengubah entry point /welcome menjadi /login

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\BarangGadaiController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
